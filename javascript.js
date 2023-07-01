6e7aabdf77ee180b6a21b571d96d79e29f808129
hansda6908 <ashishhansda21@gmail.com>
2023-07-01 20:00:56 +0530

    my Blinkit clone

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log('Javascript added');
+
+document.getElementsByClassName('login-button')[0].addEventListener('click', function () {
+    alert("Login button clicked");
+})
+
+document.getElementsByClassName('cart1')[0].addEventListener('click', function () {
+    alert("Cart Button clicked");
+})
+
+document.getElementsByClassName('add-to-cart')[0].addEventListener('click', function () {
+    alert("Item added");
+})
+
